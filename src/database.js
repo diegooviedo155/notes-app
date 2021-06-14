262ec62cdaa763852a4fb560a88954799b02ca12
Diego Alejandro Oviedo <78284195+diegooviedo155@users.noreply.github.com>
2021-06-14 12:51:29 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST || "localhost"}/${NOTES_APP_MONGODB_DATABASE||"notes-app"}`;
+//${NOTES_APP_MONGODB_DATABASE||"notes-app"}`;
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST || "localhost"}`;
 
 mongoose.connect(MONGODB_URI, {
         useNewUrlParser: true,
