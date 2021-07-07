c6fd34b33ed4007f4697d58d5073941c5d7bbbb6
Diego Alejandro Oviedo <78284195+diegooviedo155@users.noreply.github.com>
2021-07-07 12:16:37 -0300

    deje solo el URI_DB

--- src/database.js
+++ src/database.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE, URI_DB } = process.env
 
-const MONGODB_URI = `URI_DB || (mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE})`;
+//const MONGODB_URI = `URI_DB || (mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE})`;
 
+const MONGODB_URI = URI_DB;
+      
 mongoose.connect(MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
